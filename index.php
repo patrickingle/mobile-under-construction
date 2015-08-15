30e9856bfc493819ae6a260888f269d18b8f82ff
Patrick Ingle <phkcorp2005@gmail.com>
2015-08-15 16:49:21 -0400

    initial theme cloned for wordpress compatibility with static text

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Coming Soon</title>
+<link href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/tools/jquery.min.js"></script> 
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/tools/cufon-yui.js"></script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/tools/Comfortaa_250-Comfortaa_700.font.js"></script>
+<script type="text/javascript">
+	Cufon.replace('h1', {fontFamily: 'Comfortaa'});
+	Cufon.replace('p.c_soon strong', {fontFamily: 'Comfortaa'});
+</script>
+
+</head>
+<body>
+<div class="content_wrapper">
+	<div class="top_content">
+		<div class="header">
+			<div class="iphone">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/image.jpg" alt="" />
+			</div>
+			<div class="logo_content">
+				<h1><strong>drink</strong>alike</h1>
+				<h3>The <strong>EASIEST</strong> way to know, <strong>wat to drink next?</strong></h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque arcu ex, luctus sit amet blandit quis, varius consectetur justo. Vivamus consectetur diam at libero imperdiet mollis. Donec hendrerit elit at augue tristique consequat. Morbi posuere egestas convallis. Etiam pellentesque, magna vel facilisis semper, quam justo facilisis turpis, eget vehicula felis felis ac nisi. Nunc erat enim, dictum eu laoreet ac, aliquet non turpis. Aliquam imperdiet dui eu urna consectetur placerat. Nulla ut feugiat tortor.</p>
+				<a href="#" class="get_notified"><strong>Get Notified</strong> on the day of launch!</a>
+			</div>
+		</div>
+	</div>
+	
+	<div class="shadow"></div>
+	<div class="white_content">
+		<div class="top_content">
+			<div class="header">
+				<div class="coming_soon">
+					<p class="c_soon">we're <br/> <strong>Coming Soon</strong> <br/> to your iPhone, Android and Windows Phone</p>
+				</div>
+				
+				<ul class="social">
+					<li><a href="#" class="googleplus">google plus</a></li>
+					<li><a href="#" class="twitter">twitter</a></li>
+					<li><a href="#" class="facebook">facebook</a></li>
+					<li><a href="#" class="email">email</a></li>
+					<li><a href="#" class="youtube">youtube</a></li>
+					<li><a href="#" class="pinterest">pinterest</a></li>
+				</ul>
+				
+				<div class="copyright"><p>Copyright &copy; <?php echo date("Y"); ?>. <br/> All Rights Reserved. <br/> <span><strong>drink</strong>alike</span></p></div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
